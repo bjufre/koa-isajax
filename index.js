833306f9e68d0e2b7d0f9f593308b74f947b95eb
Bernat Jufré <info@behind.design>
2016-12-18 15:15:02 +0100

    Add keywords to package.json

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = function isajax() {
-    return function _isajax(context, next) {
-        context.state.xhr = context.request.get('X-Requested-With') === 'XMLHttpRequest';
-        return next();
-    }
+  return function _isajax(context, next) {
+    context.state.xhr = context.request.get('X-Requested-With') === 'XMLHttpRequest';
+    return next();
+  }
 }
