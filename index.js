7a15a79e47924ec79f9995576ae2826d14c74a48
Bernat Jufré <info@behind.design>
2016-12-18 12:24:40 +0100

    module.exports fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-export default function isajax() {
+module.exports = function isajax() {
     return async function _isajax(context, next) {
         context.state.xhr = context.request.get('X-Requested-With') === 'XMLHttpRequest';
         return await next();
